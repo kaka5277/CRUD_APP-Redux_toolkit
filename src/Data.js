f69f04cd5ce0bb9b21447fefc49f65a110df8d6c
“kaka5277” <254140626@qq.com>
2023-06-25 15:04:50 +1000

    init

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const userList=[
+  {
+    name:"tom khan",
+    email:'saf@gmail.com',
+    id:1
+  },
+  {
+    name:"lucy lin",
+    email:'lucy@gmail.com',
+    id:2
+  },
+  {
+    name:"kate wood",
+    email:'kate@gmail.com',
+    id:3
+  }
+]
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {configureStore} from '@reduxjs/toolkit'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import UserReducer  from './UserReducer';
+
+
+const store=configureStore({
+  reducer:{
+    users:UserReducer
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>
+);
+
